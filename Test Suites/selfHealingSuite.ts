0342a22824ccf178a118910aa4d66cdbcb7cbb0f
Diksha Gupta <Diksha Gupta@KTLPT1683>
2025-09-01 15:52:49 +0530

    Commit updation

--- Test Suites/selfHealingSuite.ts
+++ Test Suites/selfHealingSuite.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>ffa8bb1e-4f23-4a94-b6ea-ef0a6330b6ef</testSuiteGuid>
+   <testCaseLink>
+      <guid>a9212c7a-121a-47b9-addb-1e19a279d111</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SelfHealingDemoTests</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
