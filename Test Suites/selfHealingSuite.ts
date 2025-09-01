1837ae4b7657e18cd59536073b59b33a3ca90945
Diksha Gupta <Diksha Gupta@KTLPT1683>
2025-09-01 15:30:00 +0530

    Rename the suite name

---
+++ Test Suites/selfHealingSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>selfHealingSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ffa8bb1e-4f23-4a94-b6ea-ef0a6330b6ef</testSuiteGuid>
+</TestSuiteEntity>
